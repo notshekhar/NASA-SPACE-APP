275e61bd6314170a1805891ab3cb88aae5c198a4
Shekhar Tyagi <notshekhar@gmail.com>
2020-10-04 12:31:43 +0530

    new

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ $(".menu li").click(() => {
 })
 
 // typing text animation script
-var typed = new Typed(".typing", {
-    strings: ["Scientists", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-})
+// var typed = new Typed(".typing", {
+//     strings: ["Scientists", "Developer", "Blogger", "Designer", "Freelancer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true,
+// })
 var typed = new Typed(".typing-2", {
     strings: ["Scientists", "Developer", "Blogger", "Designer", "Freelancer"],
     typeSpeed: 100,
